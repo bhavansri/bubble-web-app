f9219b9ddb9d3ba60b924cd3b394a3b3edad4f21
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-07-12 13:42:59 -0400

    updates

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Bubble Calendar',
-  title: `You're Invited`,
+  title: `You're Invited!`,
   description: 'Social events organized in seconds',
   locale: 'en',
 };
